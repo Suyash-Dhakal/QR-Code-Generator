be25a609729a93bf4b1f7e54e5aa6c6711ea5492
Suyash Dhakal <suyash.dhakal@icloud.com>
2024-08-11 22:18:37 +0545

    feat: add download button for QR code
    - Implemented a download button to save generated QR codes
    - Added functionality to trigger QR code download as an image file
    - Updated the UI to accommodate the new button

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,17 @@
 const BASE_URL="https://api.qrserver.com/v1/create-qr-code/?";
 
 
-let btn=document.querySelector(".button-confirm");
+let btnGenerate=document.querySelector(".button-confirm");
+let btnDownload=document.querySelector("#download");
+let input=document.querySelector("form .input");
+let img=document.querySelector(".qr-img img");
 
-btn.addEventListener("click",(evt)=>{
+btnGenerate.addEventListener("click",(evt)=>{
     generateQR();
     evt.preventDefault();
 });
 
 function generateQR(){
-    let input=document.querySelector("form .input");
-    let img=document.querySelector(".qr-img img");
     img.src=`${BASE_URL}data=${input.value}&size=300x300`;
 }
 
@@ -20,3 +21,25 @@ function reset(){
     input.value="";
     img.src=""
 }
+
+btnDownload.addEventListener("click",(evt)=>{
+    evt.preventDefault();
+    downloadQR();
+});
+
+function downloadQR(){
+    img.src=`${BASE_URL}data=${input.value}&size=300x300`;
+    if(img.src!=""){
+        fetch(img.src)
+        .then(response=>response.blob())
+        .then(blob=>{
+            saveAs(blob,"generated-QR.png");
+        })
+        .catch(error=>{
+            console.log("Error saving image:",error);
+            
+        });
+
+    }
+}
+
