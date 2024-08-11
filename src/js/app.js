627fd424fe9ad091fd545c44c42cb14780458a60
Suyash Dhakal <suyash.dhakal@icloud.com>
2024-08-11 18:35:44 +0545

    feat: complete initial project version - Completed initial design and layout
    Future improvements planned:
    - Additional features
    - Design enhancements
    
    Ready for deployment.

---
+++ src/js/app.js
@@ -0,0 +1,22 @@
+const BASE_URL="https://api.qrserver.com/v1/create-qr-code/?";
+
+
+let btn=document.querySelector(".button-confirm");
+
+btn.addEventListener("click",(evt)=>{
+    evt.preventDefault();
+    generateQR();
+});
+
+function generateQR(){
+    let input=document.querySelector("form .input");
+    let img=document.querySelector(".qr-img img");
+    img.src=`${BASE_URL}data=${input.value}&size=100x100`;
+}
+
+function reset(){
+    let input=document.querySelector("form .input");
+    let img=document.querySelector(".qr-img img");
+    input.value="";
+    img.src=""
+}
